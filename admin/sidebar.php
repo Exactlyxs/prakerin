b7e5d96d7ffeb50841780295e3b7be26b038d693
KikyRed4 <rizkyafdhillah2rpl@gmail.com>
2018-12-01 17:51:23 +0800

    add magang

---
+++ admin/sidebar.php
@@ -0,0 +1,61 @@
+<html>
+
+<!-- link css and Bootsrap -->
+
+<head>
+    <title>Admin</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style_Sidebar.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Mina:700" rel="stylesheet">
+
+
+
+</head>
+
+<body>
+    <!-- SideBar Logo -->
+  <div id="wrapper" class="animate">
+    <nav class="navbar header-top fixed-top navbar-expand-lg  navbar-dark bg-dark">
+      <span class="navbar-toggler-icon leftmenutrigger"></span>
+      <a class="navbar-brand" href="#"> <img src="../assets/images/smkLogo.png" width="30"></a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- SideBar Isi -->
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav animate side-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">DASHBOARD
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">MANAGE PRAKERIN</a>
+          </li>
+        </ul>
+        <!-- NavBar -->
+        <ul class="navbar-nav ml-md-auto d-md-flex">
+          <li class="nav-item dropdown icon">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-user-circle" style="font-size: 20px;"></i>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <center>
+                <p class="dropdown-item" href="#">Riswan Noorhidayat</p>
+                <p class="dropdown-item" href="#">0123456789</p>
+              </center>
+            </div>
+          </li>
+          <li class="nav-item icon">
+            <a class="nav-link btn btn-danger btn-sm" href="#">LOGOUT</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
